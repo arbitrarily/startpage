69187067614394a9f58476ccf119dfe0339e16e3
Marko Bajlovic <mbajlovic@gmail.com>
2020-09-09 18:13:57 -0400

    updates

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 jQuery(function($) {
   $(".search").on("click", function() {
-  	console.log($("#searchform").attr('action'));
-  	if ($("#searchform").attr('action') === 'https://www.google.com/search') {
-  		var action = 'https://duckduckgo.com/',
-  			logo = 'duckduckgo.svg';
-  	} else {
-  		var action = 'https://www.google.com/search',
-  			logo = 'google.svg';
-  	}
-  	$("#searchform").attr('action', action);
-  	$(".search").attr('src', logo);
+    if ($("#searchform").attr('action') === 'https://google.com/search') {
+      var action = 'https://duckduckgo.com',
+          logo = 'duckduckgo.svg';
+    } else if ($("#searchform").attr('action') === 'https://duckduckgo.com') {
+      var action = 'https://youtube.com/results?search_query=',
+          logo = 'youtube.svg';
+    } else {
+      var action = 'https://google.com/search',
+          logo = 'google.svg';
+    }
+    $("#searchform").attr('action', action);
+    $(".search").attr('src', logo);
+  });
+  $("input").focus(function() {
+    $("form").addClass("focus");
+  });
+  $("input").focusout(function() {
+    $("form").removeClass("focus");
   });
 });
