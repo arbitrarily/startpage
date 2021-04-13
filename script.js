b3a3c2a4a90c6fc30f16018e29632df3a301b11f
Marko Bajlovic <mbajlovic@gmail.com>
2021-04-13 00:58:53 -0400

    click anywhere for input focus

--- script.js
+++ script.js
@@ -15,43 +15,51 @@ jQuery( function( $ ) {
     $( ".search" ).attr( 'src', logo );
   } );
 
-  $( "input" ).focus( function() {
+  $( "#search" ).focus( function() {
     $( "form" ).addClass( "focus" );
   } );
 
-  $( "input" ).focusout( function() {
+  $( "#search" ).focusout( function() {
     $( "form" ).removeClass( "focus" );
   } );
 
-  var lasty = setInterval(lastfm, 1000 * 60 * 3);
+  $( document ).on( "click", function( e ) {
+    if ( e.target.tagName !== "A" || e.target.tagName !== "INPUT" ) {
+      $( "#search" ).focus();
+      $( "form" ).addClass( "focus" );
+    }
+  } );
+
+
+  var lasty = setInterval( lastfm, 1000 * 60 * 3 );
   lastfm();
 
   function lastfm() {
     var url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=desmosthenes&api_key=df46fd3a07f78050c087696ec675130c&format=json&limit=1';
 
-    fetch(url)
-      .then(res => res.json())
-      .then(res => res['recenttracks']['track'])
-      .then(song => {
-        var s = song.map(_map_it)[0];
-        $(".lastfm__artist").text(s.artist);
-        $(".lastfm__song").text(s.name);
-        $(".lastfm__album").text(s.album);
-        if (s.image != "") {
-          $(".lastfm__image").attr("src", s.image).show();
+    fetch( url )
+      .then( res => res.json() )
+      .then( res => res[ 'recenttracks' ][ 'track' ] )
+      .then( song => {
+        var s = song.map( _map_it )[ 0 ];
+        $( ".lastfm__artist" ).text( s.artist );
+        $( ".lastfm__song" ).text( s.name );
+        $( ".lastfm__album" ).text( s.album );
+        if ( s.image != "" ) {
+          $( ".lastfm__image" ).attr( "src", s.image ).show();
         } else {
-          $(".lastfm__image").hide();
+          $( ".lastfm__image" ).hide();
         }
-        $(".lastfm__url").attr("href", s.link).addClass("shown");
-    });
+        $( ".lastfm__url" ).attr( "href", s.link ).addClass( "shown" );
+      } );
 
-    function _map_it(song) {
+    function _map_it( song ) {
       return {
         id: song.mbid,
         name: song.name,
-        album: song.album["#text"],
-        artist: song.artist["#text"],
-        image: song.image[3]["#text"],
+        album: song.album[ "#text" ],
+        artist: song.artist[ "#text" ],
+        image: song.image[ 3 ][ "#text" ],
         link: song.url
       }
     }
