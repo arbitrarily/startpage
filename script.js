791b8c1acba67ef367992c3edf0ba94dd423d656
Marko Bajlovic <mbajlovic@gmail.com>
2022-11-20 18:12:09 -0400

    cleanup, moved icons

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: '1.4.4',
+    version: '1.4.5',
 
     // Touch Events
     touch: "ontouchstart" in document.documentElement ? "touchstart" : "click",
