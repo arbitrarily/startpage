a5e2d877dcb0cd7f50ff1d604d3630d90ba4f3b8
Marko Bajlovic <mbajlovic@gmail.com>
2023-02-11 17:25:55 -0400

    new max width

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.8.14",
+    version: "1.8.15",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
