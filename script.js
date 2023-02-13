25e23c8f2dcd8c6c6e74931bac926c9fe1d3ee35
Marko Bajlovic <mbajlovic@gmail.com>
2023-02-13 12:32:27 -0400

    updated libs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.8.16",
+    version: "1.8.17",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
