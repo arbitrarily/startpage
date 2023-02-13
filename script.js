32630a3ed40d20743636b37053237cd4d1025ac4
Marko Bajlovic <mbajlovic@gmail.com>
2023-02-13 12:34:36 -0400

    moved icons

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.8.17",
+    version: "1.8.18",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
