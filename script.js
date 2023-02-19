a47062c6b5cbb07cfb05ecd12e4f5ea09e785315
Marko Bajlovic <mbajlovic@gmail.com>
2023-02-19 16:34:23 -0400

    new poe links

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.9.3",
+    version: "1.9.4",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
