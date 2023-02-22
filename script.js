172086107f33f02cdde1e6d102eb68f9790810b1
Marko Bajlovic <mbajlovic@gmail.com>
2023-02-21 22:29:50 -0400

    removed techmeme css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.10.14",
+    version: "1.10.15",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
