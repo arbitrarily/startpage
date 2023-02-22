19797a13cb4e4edb4a1c6c9d7fc6f547e4bd3878
Marko Bajlovic <mbajlovic@gmail.com>
2023-02-22 03:01:51 -0400

    news feed css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.10.17",
+    version: "1.10.18",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
