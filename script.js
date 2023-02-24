007ea220a872e3d7e04bf45197bd210f9936a943
Marko Bajlovic <mbajlovic@gmail.com>
2023-02-23 21:54:39 -0400

    large toggle tweaks

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.10.29",
+    version: "1.10.30",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
