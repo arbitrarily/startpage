c89972f2204041c6b07cedd526bafbe5e3823217
Marko Bajlovic <mbajlovic@gmail.com>
2023-02-28 15:41:08 -0400

    aspect ratio on lastfm img

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.10.35",
+    version: "1.10.36",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
