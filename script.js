05a73b6ee3a7577c60751f61eefdb24d316dcdc4
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-03 04:00:21 -0400

    fallback styling

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.11.17",
+    version: "1.11.18",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
