404c1d68ca6c4b0204c28df58a9d7fa5ec0a4184
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-03 03:09:59 -0400

    padding on large view

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.11.15",
+    version: "1.11.16",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
