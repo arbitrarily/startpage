03a9182ac2047e48e59180dadef3b0114f6d3488
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-04 19:46:41 -0400

    mobile nav 2

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.14.4",
+    version: "1.14.5",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
