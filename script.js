97e012a40e7782a606146fd495d57d1eabfeb9c5
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-04 22:27:48 -0400

    new icons

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.14.15",
+    version: "1.14.16",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
