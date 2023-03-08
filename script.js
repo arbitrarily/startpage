f44a0afe8596616daf65291bb4e181f77fc51bcb
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-07 23:04:16 -0400

    video updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.15.20",
+    version: "1.15.21",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
