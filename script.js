3321b22b0058092784d2bcf8b0bd39b43d98e385
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-07 23:33:33 -0400

    nft mobile view

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.15.22",
+    version: "1.15.23",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
