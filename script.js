2b479583347155bff1fdaab5ac377ff63ca2d638
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-07 22:48:29 -0400

    remove shadow animation, spins up computer fan

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.15.19",
+    version: "1.15.20",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
