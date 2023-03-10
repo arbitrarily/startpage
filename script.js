f772efe4002d49d9ea61fdbde662a6476df96380
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-09 23:39:47 -0400

    nft feed styling updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.16.12",
+    version: "1.16.13",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
