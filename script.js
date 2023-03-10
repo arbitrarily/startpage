53c08eea96a70f54ade675a0322e10d81856bf3e
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-09 21:01:54 -0400

    updated css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.16.7",
+    version: "1.16.8",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
