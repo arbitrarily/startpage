6d359c266a67223f61e91693b3316417ce10060d
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-09 23:19:49 -0400

    removed unused css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.16.11",
+    version: "1.16.12",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
