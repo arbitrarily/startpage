b02d8a1184a502720927ce1d5e7deddef0dac5c4
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-11 20:32:30 -0400

    ui updates

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var start = {
 
     // Version Number
-    version: "1.18.4",
+    version: "1.18.5",
 
     // Touch Events
     touch: "onontouchend" in document.documentElement ? "ontouchend" : "click",
