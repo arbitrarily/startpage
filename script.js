9be9596e0492e6e21b597e3e5995d1bdeb621de8
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-20 23:00:55 -0400

    min width on nowplaying image

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
     s: "shown", // Shared Class Names
     t: "ontouchend" in document.documentElement || "click", // Touch Events
     timer: {}, // Timer Count
-    v: "1.24.2", // Version Number
+    v: "1.24.3", // Version Number
     vaa: false, // Video as Audio
     video: false, // Video
 
