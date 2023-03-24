839cc5348449298daa29f0782e1a05bb8ac67183
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-24 18:00:35 -0400

    action updated

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
     s: "shown", // Shared Class Names
     t: "ontouchend" in document.documentElement || "click", // Touch Events
     timer: {}, // Timer Count
-    v: "1.28.3", // Version Number
+    v: "1.28.4", // Version Number
     vaa: false, // Video as Audio
     video: false, // Video
 
