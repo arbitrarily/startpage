cd3c11bb2792ddfc462d06e80dadd39c4d3e8a59
Marko Bajlovic <mbajlovic@gmail.com>
2023-03-25 05:06:48 -0400

    video in nfts styling

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
     s: "shown", // Shared Class Names
     t: "ontouchend" in document.documentElement || "click", // Touch Events
     timer: {}, // Timer Count
-    v: "1.29.4", // Version Number
+    v: "1.29.5", // Version Number
     vaa: false, // Video as Audio
     video: false, // Video
 
