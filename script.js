7b165f32dc60acfc42ca90ae4812ee76b20cab27
Marko Bajlovic <mbajlovic@gmail.com>
2023-04-02 22:00:47 -0400

    padding fixes to feed titles

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
     s: "shown", // Shared Class Names
     t: "ontouchstart" in window || navigator.msMaxTouchPoints ? "touchend" : "click", // Touch Events
     timer: {}, // Timer Count
-    v: "1.39.8", // Version Number
+    v: "1.39.9", // Version Number
     vaa: false, // Video as Audio
     video: false, // Video
 
