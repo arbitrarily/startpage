ef09162f6b293f8fccd32e2fdb13efc1cb813b3a
Marko Bajlovic <mbajlovic@gmail.com>
2023-04-04 15:44:42 -0400

    lineclamp on music links

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
     s: "shown",           // Shared Class Names
     t: "click",           // Touch Events
     timer: {},            // Timer Count
-    v: "1.41.6",          // Version Number
+    v: "1.41.7",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
