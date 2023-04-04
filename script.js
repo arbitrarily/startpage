690b1b5c9b68c8af07db5de6e960931b7a5a148f
Marko Bajlovic <mbajlovic@gmail.com>
2023-04-04 00:23:11 -0400

    updated jquery

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
     s: "shown", // Shared Class Names
     t: "click", // Touch Events
     timer: {}, // Timer Count
-    v: "1.40.12", // Version Number
+    v: "1.40.13", // Version Number
     vaa: false, // Video as Audio
     video: false, // Video
 
