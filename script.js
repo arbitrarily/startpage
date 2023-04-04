e71c56062d04f62e0b3356783a88e2d41b75663a
Marko Bajlovic <mbajlovic@gmail.com>
2023-04-04 00:09:39 -0400

    new icons, new links

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
     s: "shown", // Shared Class Names
     t: "click", // Touch Events
     timer: {}, // Timer Count
-    v: "1.40.11", // Version Number
+    v: "1.40.12", // Version Number
     vaa: false, // Video as Audio
     video: false, // Video
 
