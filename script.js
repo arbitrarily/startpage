662c02cd9ca9228906b78e4320d6f4da52631280
Marko Bajlovic <mbajlovic@gmail.com>
2023-06-19 00:11:47 -0400

    responsive css updates

--- script.js
+++ script.js
@@ -35,7 +35,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.54.10",          // Version Number
+    v: "1.54.11",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
