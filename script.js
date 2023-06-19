5005717930ee6d283e3b0857cf0ae1016155a269
Marko Bajlovic <mbajlovic@gmail.com>
2023-06-18 21:07:25 -0400

    polish, refactoring css

--- script.js
+++ script.js
@@ -35,7 +35,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.54.6",          // Version Number
+    v: "1.54.7",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
