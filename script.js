92b0b6535fc24a6d5fee020630bd0dcb839229d6
Marko Bajlovic <mbajlovic@gmail.com>
2023-06-21 03:22:18 -0400

    summaries fullscreen styling

--- script.js
+++ script.js
@@ -36,7 +36,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.55.5",          // Version Number
+    v: "1.55.6",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
