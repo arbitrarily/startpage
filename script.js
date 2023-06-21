f5a273a52bb63141e9eed22da640799429f6fefc
Marko Bajlovic <mbajlovic@gmail.com>
2023-06-21 04:22:55 -0400

    now playing fade animation

--- script.js
+++ script.js
@@ -36,7 +36,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.55.7",          // Version Number
+    v: "1.55.8",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
