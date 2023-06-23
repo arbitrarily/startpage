7c8bb9fe15a7984d90c1061460488564f5d62d4e
Marko Bajlovic <mbajlovic@gmail.com>
2023-06-22 21:47:59 -0400

    css animation update 2

--- script.js
+++ script.js
@@ -36,7 +36,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.56.2b",          // Version Number
+    v: "1.56.3",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
