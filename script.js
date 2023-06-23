09b76a77ee27d06f39cec8cc1f9080f392603369
Marko Bajlovic <mbajlovic@gmail.com>
2023-06-22 21:42:59 -0400

    progress bar animation

--- script.js
+++ script.js
@@ -36,7 +36,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.56.1",          // Version Number
+    v: "1.56.2",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
