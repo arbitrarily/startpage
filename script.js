2d74df006162cd9f7e318de6f44a75fd5d4fbbc2
Marko Bajlovic <mbajlovic@gmail.com>
2023-06-25 23:30:49 -0400

    responsive updates for tablet menu view

--- script.js
+++ script.js
@@ -32,7 +32,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.59.10",         // Version Number
+    v: "1.59.11",         // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
