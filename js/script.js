b25a785961d1467d7057443c1f0541a1e1208317
Marko Bajlovic <mbajlovic@gmail.com>
2023-07-05 19:16:28 -0400

    minor refactoring

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.61.5",          // Version Number
+    v: "1.61.6",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
