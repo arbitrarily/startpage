de0cf9c0ebea484757870900d22b21fd83da971b
Marko Bajlovic <mbajlovic@gmail.com>
2023-07-06 19:01:34 -0400

    added mastodon link

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@
     title: 'Startpage',   // Page Title
     ti: false,            // Page Title Interval
     timer: {},            // Timer Count
-    v: "1.62.5",          // Version Number
+    v: "1.62.6",          // Version Number
     vaa: false,           // Video as Audio
     video: false,         // Video
 
