5bb888d4aad998359c71403e1dc5a6d959551f42
Marko Bajlovic <mbajlovic@gmail.com>
2023-08-28 23:04:14 -0400

    search redesigned

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@
     title: "Startpage",          // Page Title
     ti: false,                   // Page Title Interval
     timer: {},                   // Timer Count
-    v: "1.63.1",                 // Version Number
+    v: "1.63.2",                 // Version Number
     vaa: false,                  // Video as Audio
     video: false,                // Video
 
